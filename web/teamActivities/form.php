7f65cdfcbcc52f5604ce3efa2fa9cb72fc3188a2
wesleygdike <wesley.g.dike@gmail.com>
2017-09-27 17:41:05 -0600

    Team Project 2.0 092717

---
+++ web/teamActivities/form.php
@@ -0,0 +1,12 @@
+﻿<html>
+<head>
+    <title>Team page form</title>
+</head>
+<body>
+    <h1>Here is the webpage we generated</h1>
+    <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["email"]; ?><br>
+    <?php echo $_POST["major"]; ?><br>
+    <?php echo $_POST["comments"]; ?><br>
+</body>
+</html>
---
+++ web/teamActivities/teamProject02.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Team Project</title>
+</head>
+<body>
+    <form action="form.php" method="post">
+        Name<input type="text" name="name"><br>
+        Email<input type="text" name="email">
+        <br>
+        Major
+        <br>
+        <input type="radio" name="major">Computer Science<br>
+        <input type="radio" name="major">Web Design and Development<br>
+        <input type="radio" name="major">Computer Informaion Technology<br>
+        <input type="radio" name="major">Computer Engineering
+        <br>Comments<br>
+        <textarea rows="4" cols="50" name="comments"></textarea>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
