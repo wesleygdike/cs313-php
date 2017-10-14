d6eca254248c7492f5160276428b9fecded1e10d
wesleygdike <wesley.g.dike@gmail.com>
2017-10-14 17:52:08 -0600

    assignment 05.02	101417

--- web/asteriodanon/SQLdata.sql
+++ web/asteriodanon/SQLdata.sql
@@ -3,14 +3,6 @@
  * Created: Oct 5, 2017
  */
 
-CREATE TABLE user (
-user_id     int PRIMARY KEY,
-user_name   varchar(28) NOT NULL,
-ship        int REFERENCES ships(ship_id),
-user_input  int REFERENCES inputs(input_id),
-user_state  int
-);
-
 CREATE TABLE flying_Object (
 obj_id     SERIAL PRIMARY KEY NOT NULL,
 xloc        int, --set min max for all locations
@@ -22,23 +14,9 @@ is_alive    boolean,
 obj_type    int  --ASTEROID=0, SHIP=1
 );
 
-CREATE TABLE ship (
-ship_id     int PRIMARY KEY,
-my_user     int REFERENCES users(user_id),
-score       int NOT NULL,
-flyingObject    int REFERENCES flyingObject(obj_id),
-bullets     int REFERENCES bullets(bullet_list_id)
-);
-
 CREATE TABLE asteroid (
 asteroid_id int PRIMARY KEY,
 point_value int,
-flyingObject    int REFERENCES flyingObject(obj_id)
-);
-
-CREATE TABLE asteroid (
-bullet_id int PRIMARY KEY,
-life_counter    int,
 flyingObject    int REFERENCES flyingObject(obj_id)
 );
 
@@ -47,6 +25,16 @@ input_id
 booster int,
 turn    int,
 fire    int
+);
+
+CREATE TABLE user (
+user_id     int PRIMARY KEY,
+user_name   varchar(28) NOT NULL,
+score       int NOT NULL,
+flyingObject    int REFERENCES flyingObject(obj_id),
+bullets     int REFERENCES bullets(bullet_list_id)
+user_input  int REFERENCES inputs(input_id),
+user_state  int
 );
 
 CREATE TABLE game (
