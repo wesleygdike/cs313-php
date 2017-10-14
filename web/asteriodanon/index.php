271b1ed1c5ebed72e93150ab511ec0267f9b8ea3
wesleygdike <wesley.g.dike@gmail.com>
2017-10-14 17:36:33 -0600

    Assignment 05.00	101417

--- web/asteriodanon/index.php
+++ web/asteriodanon/index.php
@@ -9,6 +9,26 @@
         <div id="canvasholder">
             <!-- Canvas will go here -->
         </div>
+        <?php
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbopts = parse_url($dbUrl);
+
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"], '/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        echo "<h2>FlyingObjects:</h2>";
+
+        foreach ($db->query('SELECT * FROM flying_object') as $row)
+        {
+                echo '<div><b>' . $row['book'] . ' '. $row['chapter'] . ":" . $row['verse'] . '</b> - "' . $row['content'] . '"';
+        }
+        ?>
         <input type="submit" value="Move" name="moveRightBtn" />
     </body>
 </html>
