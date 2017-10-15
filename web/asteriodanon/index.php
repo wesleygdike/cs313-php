a0a186d1b624394e3d18b9f5c7a8e873bd224597
wesleygdike <wesley.g.dike@gmail.com>
2017-10-14 18:02:47 -0600

    assignement 05.07 101417

--- web/asteriodanon/index.php
+++ web/asteriodanon/index.php
@@ -23,12 +23,35 @@
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
         echo "<h2>FlyingObjects:</h2>";
+//make the table header
+echo '<div>
+    <table>
+    <tr>
+    <th>Object Id</th>
+    <th>X location</th> 
+    <th>Y location</th>
+    <th>X velocity</th>
+    <th>Y velocity</th>
+    <th>Rotation</th>
+    <th>Is Alive</th>
+    <th>Object Type Identifier</th>
+  </tr>';
 
-        foreach ($db->query('SELECT * FROM flying_object') as $row)
-        {
-                echo '<div><b>' . $row['book'] . ' '. $row['chapter'] . ":" . $row['verse'] . '</b> - "' . $row['content'] . '"';
-        }
-        ?>
+foreach ($db->query('SELECT * FROM scriptures') as $row)
+{
+    //add a row to the table for each flying_object
+	echo '<tr><td>' . $row['obj_id'] . '</td>'. 
+                '<td>' . $row['obj_id'] . '</td>'.
+                '<td>' . $row['xloc'] . '</td>'.
+                '<td>' . $row['yloc'] . '</td>'.
+                '<td>' . $row['xvel'] . '</td>'.
+                '<td>' . $row['yvel'] . '</td>'.
+                '<td>' . $row['rotation'] . '</td>'.
+                '<td>' . $row['is_alive'] . '</td>'.
+                '<td>' . $row['obj_type'] . '</td></tr>';
+}
+?>
+
         <input type="submit" value="Move" name="moveRightBtn" />
     </body>
 </html>
