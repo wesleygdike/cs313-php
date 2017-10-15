5efcf7e93199b6a971f0e98e83a7421be56380a7
wesleygdike <wesley.g.dike@gmail.com>
2017-10-14 18:07:55 -0600

    assignment 05.09 101417

--- web/asteriodanon/index.php
+++ web/asteriodanon/index.php
@@ -5,7 +5,7 @@
         <title>Asteriodanon</title>
     <script src="astroScript.js"></script>
     </head>
-    <body onload="startGame()">
+    <body onload="/*startGame()*/">
         <div id="canvasholder">
             <!-- Canvas will go here -->
         </div>
@@ -23,34 +23,34 @@
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
         echo "<h2>FlyingObjects:</h2>";
-//make the table header
-echo '<div>
-    <table>
-    <tr>
-    <th>Object Id</th>
-    <th>X location</th> 
-    <th>Y location</th>
-    <th>X velocity</th>
-    <th>Y velocity</th>
-    <th>Rotation</th>
-    <th>Is Alive</th>
-    <th>Object Type Identifier</th>
-  </tr>';
+        //make the table header
+        echo '<div>
+            <table>
+            <tr>
+            <th>Object Id</th>
+            <th>X location</th> 
+            <th>Y location</th>
+            <th>X velocity</th>
+            <th>Y velocity</th>
+            <th>Rotation</th>
+            <th>Is Alive</th>
+            <th>Object Type Identifier</th>
+          </tr>';
 
-foreach ($db->query('SELECT * FROM scriptures') as $row)
-{
-    //add a row to the table for each flying_object
-	echo '<tr><td>' . $row['obj_id'] . '</td>'. 
-                '<td>' . $row['obj_id'] . '</td>'.
-                '<td>' . $row['xloc'] . '</td>'.
-                '<td>' . $row['yloc'] . '</td>'.
-                '<td>' . $row['xvel'] . '</td>'.
-                '<td>' . $row['yvel'] . '</td>'.
-                '<td>' . $row['rotation'] . '</td>'.
-                '<td>' . $row['is_alive'] . '</td>'.
-                '<td>' . $row['obj_type'] . '</td></tr>';
-}
-?>
+        foreach ($db->query('SELECT * FROM flying_objects') as $row)
+        {
+            //add a row to the table for each flying_object
+                echo '<tr><td>' . $row['obj_id'] . '</td>'. 
+                        '<td>' . $row['obj_id'] . '</td>'.
+                        '<td>' . $row['xloc'] . '</td>'.
+                        '<td>' . $row['yloc'] . '</td>'.
+                        '<td>' . $row['xvel'] . '</td>'.
+                        '<td>' . $row['yvel'] . '</td>'.
+                        '<td>' . $row['rotation'] . '</td>'.
+                        '<td>' . $row['is_alive'] . '</td>'.
+                        '<td>' . $row['obj_type'] . '</td></tr>';
+        }
+        ?>
 
         <input type="submit" value="Move" name="moveRightBtn" />
     </body>
