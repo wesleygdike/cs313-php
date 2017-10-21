51780176f80d30a8108249489e033510f7284a28
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 14:05:43 -0600

    data base connection test

--- web/asteriodanon/blocks/test.php
+++ web/asteriodanon/blocks/test.php
@@ -2,7 +2,26 @@
     <h3 class="margin">Testing</h3><br>
     <div class="row">
         <div class="col-sm-4">
-            
+            <?php 
+                $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"], '/');
+
+    try{
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PDOException $ex) {
+        echo 'Failed Database Connection';
+        //Display error with error page
+    }
+    echo $db;
+            ?>
         </div>
     </div>
 </div>
