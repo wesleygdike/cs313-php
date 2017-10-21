0973ec80322b35ad4b6ec5865a682f3998438534
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 12:17:13 -0600

    ast

--- web/asteriodanon/index.php
+++ web/asteriodanon/index.php
@@ -5,11 +5,12 @@ include __DIR__ . '/blocks/head.php';
 include __DIR__ . '/blocks/header.php';
 ?>
     <body>
-
+        <!-- Side Bar Section -->
         <!-- Login Section -->
         <?php include __DIR__ . '/blocks/login.php'; ?>
         <!-- Instructions Section -->
         
+        <!-- Footer Section -->
         <?php include __DIR__ . '/blocks/footer.php'; ?>
     </body>
 </html>
--- web/asteriodanon/view/asteroidanon.php
+++ web/asteriodanon/view/asteroidanon.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en-us">
 <?php include __DIR__ . '/../blocks/head.php';?>
-    
+    <body>
         <!-- Header Section -->
         <?php include __DIR__ . '/../blocks/header.php'; ?>
         <!-- Game info / canvas -->
         <?php require_once __DIR__ . '/../controller/info.php'; ?>
         <?php /*displayFlyingObj();*/ ?>
         <?php include __DIR__ . '/../blocks/footer.php'; ?>        
-    
+    </body>
 </html>
