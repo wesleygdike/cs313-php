8fa8a17afd7a345fb80c1efdec0ff540a115e4c1
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 12:43:16 -0600

    ast

--- web/asteriodanon/view/asteroidanon.php
+++ web/asteriodanon/view/asteroidanon.php
@@ -5,7 +5,7 @@
         <!-- Header Section -->
         <?php include __DIR__ . '/../blocks/header.php'; ?>
         <!-- Game info / canvas -->
-        <?php include __DIR__ . '/../blocks/tables/flyingObject-table.php' ?>
+        <?php include __DIR__ . '/../blocks/tables/flyingObject-table.php'; ?>
         <?php include __DIR__ . '/../blocks/footer.php'; ?>        
     </body>
 </html>
