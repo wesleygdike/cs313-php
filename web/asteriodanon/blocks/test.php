f5433fb3417e76516b44ca433e4d17525eb5c91c
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 16:21:54 -0600

    Using my original code

--- web/asteriodanon/blocks/test.php
+++ web/asteriodanon/blocks/test.php
@@ -3,27 +3,45 @@
     <div class="row">
         <div class="col-sm-4">
             <?php 
-            	$db = NULL;
-		// default Heroku Postgres configuration URL
-		$dbUrl = getenv('DATABASE_URL');
-		if (!isset($dbUrl) || empty($dbUrl)) {
-			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
+        $dbUrl = getenv('DATABASE_URL');
 
-		$dbopts = parse_url($dbUrl);
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
-		// Create the PDO connection
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	$statement = $db->prepare('SELECT * FROM flying_object');
-	$statement->execute();
-	// Go through each result
-	$row = $statement->fetch(PDO::FETCH_ASSOC);
-        echo var_dump($row);
+        $dbopts = parse_url($dbUrl);
+
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"], '/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        echo "<h2>FlyingObjects:</h2>";
+        //make the table header
+        echo '<div>
+            <table>
+            <tr>
+            <th>Object Id</th>
+            <th>X location</th> 
+            <th>Y location</th>
+            <th>X velocity</th>
+            <th>Y velocity</th>
+            <th>Rotation</th>
+            <th>Is Alive</th>
+            <th>Object Type Identifier</th>
+          </tr>';
+
+        foreach ($db->query('SELECT * FROM flying_object;') as $row)
+        {
+            //add a row to the table for each flying_object
+                echo '<tr><td>' . $row['obj_id'] . '</td>'. 
+                        '<td>' . $row['xloc'] . '</td>'.
+                        '<td>' . $row['yloc'] . '</td>'.
+                        '<td>' . $row['xvel'] . '</td>'.
+                        '<td>' . $row['yvel'] . '</td>'.
+                        '<td>' . $row['rotation'] . '</td>'.
+                        '<td>' . $row['is_alive'] . '</td>'.
+                        '<td>' . $row['obj_type'] . '</td></tr>';
+        }
         ?>
         </div>
     </div>
