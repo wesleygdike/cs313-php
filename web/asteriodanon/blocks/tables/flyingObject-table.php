e5f34a2e9fdb590e24b8d1961f7175f814db934b
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 16:50:31 -0600

    reconfigured table code to html

--- web/asteriodanon/blocks/tables/flyingObject-table.php
+++ web/asteriodanon/blocks/tables/flyingObject-table.php
@@ -1,27 +1,25 @@
-      <?php  
-echo "<h2>FlyingObjects:</h2>";
-        //make the table header
-        echo '<table>
-            <tr>
-            <th>Object Id</th>
-            <th>X location</th> 
-            <th>Y location</th>
-            <th>X velocity</th>
-            <th>Y velocity</th>
-            <th>Rotation</th>
-            <th>Is Alive</th>
-            <th>Object Type Identifier</th>
-          </tr>';
+ 
+      <div class="row">
+        <div class="col-sm-6 form-group center">
 
-        foreach ($db->query('SELECT * FROM flying_object;') as $row)
-        {
-            //add a row to the table for each flying_object
-                echo '<tr><td>' . $row['obj_id'] . '</td>'. 
-                        '<td>' . $row['xloc'] . '</td>'.
-                        '<td>' . $row['yloc'] . '</td>'.
-                        '<td>' . $row['xvel'] . '</td>'.
-                        '<td>' . $row['yvel'] . '</td>'.
-                        '<td>' . $row['rotation'] . '</td>'.
-                        '<td>' . $row['is_alive'] . '</td>'.
-                        '<td>' . $row['obj_type'] . '</td></tr>';
-        }
+    <h2>Flying Objects:</h2>"
+    <!--make the table header-->
+    <table>
+        <tr><th>Object Id</th><th>X location</th><th>Y location</th>
+        <th>X velocity</th><th>Y velocity</th><th>Rotation</th>
+        <th>Is Alive</th><th>Object Type Identifier</th></tr>
+
+        <?php foreach($db->query('SELECT * FROM flying_object;') as $row) {?>
+            <!--add a row to the table for each flying_object-->
+            <tr><td><?php echo $row['obj_id']; ?></td>
+                <td><?php echo  $row['xloc']; ?></td>
+                <td><?php echo  $row['yloc']; ?></td>
+                <td><?php echo  $row['xvel']; ?></td>
+                <td><?php echo  $row['yvel']; ?></td>
+                <td><?php echo  $row['rotation']; ?></td>
+                <td><?php echo  $row['is_alive']; ?></td>
+                <td><?php echo  $row['obj_type']; ?></td></tr>
+        <?php } ?>
+    </table>
+        </div>
+      </div>
--- web/asteriodanon/blocks/test.php
+++ web/asteriodanon/blocks/test.php
@@ -2,11 +2,10 @@
     <h3 class="margin">Testing</h3><br>
     <div class="row">
         <div class="col-sm-4">
-            <?php 
-            include __DIR__ . '/../controller/database-service.php';
-            $db = databaseConn();
-            include __DIR__ . '/../blocks/tables/flyingObject-table.php';
+            <?php include __DIR__ . '/../controller/database-service.php';
+            $db = databaseConn(); 
             ?>
+            <?php include __DIR__ . '/../blocks/tables/flyingObject-table.php';?>
         </div>
     </div>
 </div>
