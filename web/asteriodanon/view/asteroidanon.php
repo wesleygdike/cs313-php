b1ab6acbe6bdebfc488133b52cd908f888ba2bfe
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 09:03:56 -0600

    ast 102117

--- web/asteriodanon/view/asteroidanon.php
+++ web/asteriodanon/view/asteroidanon.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-us">
 <?php include __DIR__ . '/../blocks/head.php';?>
     <body>
         <!-- Header Section -->
-        <?php include __DIR__ . '/../asteriodanon/blocks/header.php'; ?>
+        <?php include __DIR__ . '/../blocks/header.php'; ?>
         <!-- Game info / canvas -->
         <?php 
         include __DIR__ . '/../controller/info.php'; 
