d2ea0914fdce43b20ada6c03d99fc890c4dcfa78
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 16:03:43 -0600

    Trying display

--- web/asteriodanon/blocks/test.php
+++ web/asteriodanon/blocks/test.php
@@ -3,30 +3,37 @@
     <div class="row">
         <div class="col-sm-4">
             <?php 
-            
-                $dbUrl = getenv('DATABASE_URL');
+            	$db = NULL;
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
 
-    $dbopts = parse_url($dbUrl);
-
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"], '/');
-
-    try{
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch (PDOException $ex) {
-        echo 'Failed Database Connection';
-        //Display error with error page
-    }
-    echo var_dump($db);
-    $stmt = $db->prepare('SELECT * FROM flying_object');
-    $info = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo var_dump($info);
-            ?>
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	} 
+try{
+	$statement = $db->prepare('SELECT * FROM flying_object');
+	$statement->execute();
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<p>';
+		echo '<strong>' . $row['obj_id'] . ' ' . $row['xloc'] . ':';
+		echo $row['yloc'] . '</strong>' . ' - ' . $row['xvel'];
+		echo '<br />';
+		echo '</p>';
+	}
+        ?>
         </div>
     </div>
 </div>
