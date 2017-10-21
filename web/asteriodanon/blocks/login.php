0243fe875be42d2b4e99bbce1a34b7d0e5ec171c
wesleygdike <wesley.g.dike@gmail.com>
2017-10-20 22:35:49 -0600

    ast 102017

--- web/asteriodanon/blocks/login.php
+++ web/asteriodanon/blocks/login.php
@@ -3,7 +3,7 @@
     <form action="asteroidanon.php" method="post">
     <h3 class="margin">Login</h3><br>
       <div class="row">
-        <div class="col-sm-6 form-group">
+        <div class="col-sm-6 form-group center">
           <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
         </div>
       </div>
