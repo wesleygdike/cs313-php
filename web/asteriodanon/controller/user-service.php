2afb68c52a9491f0d7ed3bcc45e321942295b019
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 21:10:51 -0600

    Trying exec($sql)

--- web/asteriodanon/controller/user-service.php
+++ web/asteriodanon/controller/user-service.php
@@ -4,10 +4,9 @@ require_once __DIR__ . '/../controller/database-service.php';
 
 function createUser($userName){
     $db = databaseConn();
-    $sql = "INSERT INTO users (user_name, score, state) VALUES (?, 10, 1)";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam($userName);
-    if ($stmt->execute() === TRUE) {
+    $sql = "INSERT INTO users (user_name, score, state) VALUES (".
+            $userName . ", 10, 1)";
+    if ($db->exec($sql) === TRUE) {
         echo 'User Created: ' . $userName;
     } else {
         echo 'NOT Able To Create User: ' . $userName;
