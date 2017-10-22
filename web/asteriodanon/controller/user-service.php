f1114db75824558872e9918abd4382f99fe0df45
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 21:05:22 -0600

    Using $stmt in place of $sql

--- web/asteriodanon/controller/user-service.php
+++ web/asteriodanon/controller/user-service.php
@@ -7,7 +7,7 @@ function createUser($userName){
     $sql = "INSERT INTO users (user_name, score, state) VALUES (?, 10, 1)";
     $stmt = $db->prepare($sql);
     $stmt->bindParam($userName);
-    if ($db->query($sql) === TRUE) {
+    if ($db->query($stmt) === TRUE) {
         echo 'User Created: ' . $userName;
     } else {
         echo 'NOT Able To Create User: ' . $userName;
