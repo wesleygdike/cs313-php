0bfd97207dbe736e8edf721963eb350069863de8
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 21:23:39 -0600

    Changed to a test user string name

--- web/asteriodanon/controller/user-service.php
+++ web/asteriodanon/controller/user-service.php
@@ -4,8 +4,7 @@ require_once __DIR__ . '/../controller/database-service.php';
 
 function createUser($userName){
     $db = databaseConn();
-    $sql = "INSERT INTO users (user_name, score, state) VALUES (".
-            $userName . ", 10, 1)";
+    $sql = "INSERT INTO users (user_name, score, state) VALUES (\"Test User\", 10, 1)";
     try{
         if($db->exec($sql) === TRUE){
             echo 'User Created: ' . $userName;
