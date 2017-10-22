d241672de2054303facde867f3062d6974b21b63
wesleygdike <wesley.g.dike@gmail.com>
2017-10-21 20:49:04 -0600

    teseting insert function

--- web/asteriodanon/controller/user-service.php
+++ web/asteriodanon/controller/user-service.php
@@ -3,7 +3,13 @@
 require_once __DIR__ . '/../controller/database-service.php';
 
 function createUser($userName){
-    echo 'User Created: ' . $userName;
+    $db = databaseConn();
+    $sql = "INSERT INTO users (user_name, score, state) VALUES ($userName, 10, 1)";
+    if ($db->query($sql) === TRUE) {
+        echo 'User Created: ' . $userName;
+    } else {
+        echo 'NOT Able To Create User: ' . $userName;
+    }
 }
 
 function validateUserInput($data){
