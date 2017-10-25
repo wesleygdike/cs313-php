0b1fce2b035e5bda89a9e7a9bdb35a4140cbe8d8
wesleygdike <wesley.g.dike@gmail.com>
2017-10-24 19:27:53 -0600

    added almost full file path for clear-table.php

--- web/asteriodanon/controller/astroScript.js
+++ web/asteriodanon/controller/astroScript.js
@@ -7,6 +7,6 @@ function removeUsers() {
      //document.getElementById("demo").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "/operations/clear-table.php", true);
+  xhttp.open("GET", "asteroidanon/controller/operations/clear-table.php", true);
   xhttp.send();
 }
