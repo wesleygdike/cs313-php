6f044d84b212bd082ae7686da9d544eb52a39746
wesleygdike <wesley.g.dike@gmail.com>
2017-10-24 23:11:16 -0600

    correct input

--- web/asteriodanon/controller/astroScript.js
+++ web/asteriodanon/controller/astroScript.js
@@ -11,5 +11,14 @@ $(document).ready(function(){
 
 function keyChanged(event) {
 map[event.key] = event.type == 'keydown';
-$("#fireValue").text("W Key: " + map['w'] + " -  E Key: " + map['e']);
+$("#fireValue").text(map[' ']);
+$("#boosterValue").text(map['w']);
+var turn = 0;
+if (map['a']) {
+    turn -= 1;
+}
+if (map['d']) {
+    turn += 1;
+}
+$("#turnValue").text(turn);
 }
